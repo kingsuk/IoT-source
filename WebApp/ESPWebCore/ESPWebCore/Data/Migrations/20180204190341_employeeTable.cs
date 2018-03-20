085a16fc70a59d42237e0a5ec7abe170ebe93b0f
kingsuk <kingsuk.majumder@gmail.com>
2018-03-20 11:28:33 +0530

    core experimental changes

---
+++ WebApp/ESPWebCore/ESPWebCore/Data/Migrations/20180204190341_employeeTable.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ESPWebCore.Data.Migrations
+{
+    public partial class employeeTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(maxLength: 255, nullable: false),
+                    salary = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MyProperty", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
---
+++ WebApp/ESPWebCore/ESPWebCore/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESPWebCore.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string name { get; set; }
+
+        public double salary { get; set; }
+    }
+}
